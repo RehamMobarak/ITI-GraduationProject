02a4e05135b888e3ab901f35a0e13c70ae24cef9
aya_gaber <ayahgaber22@gmail.com>
2020-02-13 21:05:43 +0200

    Add the chatbot

---
+++ resources/views/chatbot.blade.php
@@ -0,0 +1,46 @@
+
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Careery</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.3/jquery.mCustomScrollbar.min.css'>
+
+      <link rel="stylesheet" href="style.css">
+
+
+  
+<div class="chat">
+  <div class="chat-title">
+    <h1>Careery</h1>
+    <h2>Predict the Future</h2>
+    <figure class="avatar">
+      <img src="logo.png" width="1000px" /></figure>
+      
+	  <div  class="r-nav"> 
+     <ul>
+		  <li> <a href="home.html">Back</a></li>
+		  
+		 
+     </ul>
+     
+     </div>
+      
+      
+  </div>
+  <div class="messages">
+    <div class="messages-content"></div>
+  </div>
+  <div class="message-box">
+    <textarea type="text" class="message-input" placeholder="Type message..."></textarea>
+    <button type="submit" class="message-submit sound-on-click">Send</button>
+  </div>
+
+</div>
+
